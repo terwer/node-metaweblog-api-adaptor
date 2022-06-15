04991a0027ed6f725867946ca1481e13674a2357
terwer <youweics@163.com>
2022-06-15 21:10:02 +0800

    feat:fix security issue

--- pages/docs/index.jsx
+++ pages/docs/index.jsx
@@ -5,7 +5,10 @@ export default function Home() {
             <p>
                 This is docs for
                 &nbsp;
-                <a style={{color: "blue"}} href="https://github.com/terwer/node-metaweblog-api-adaptor" target="_blank">
+                <a style={{color: "blue"}}
+                   rel="noreferrer"
+                   href="https://github.com/terwer/node-metaweblog-api-adaptor"
+                   target="_blank">
                     node-metaweblog-api-adaptor
                 </a>
             </p>
