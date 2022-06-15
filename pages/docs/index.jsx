788091be309e13aa0ffed8373d38392c70ccd038
terwer <youweics@163.com>
2022-06-15 21:07:50 +0800

    feat:getUsersBlogs api is oka

---
+++ pages/docs/index.jsx
@@ -0,0 +1,14 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <p>
+                This is docs for
+                &nbsp;
+                <a style={{color: "blue"}} href="https://github.com/terwer/node-metaweblog-api-adaptor" target="_blank">
+                    node-metaweblog-api-adaptor
+                </a>
+            </p>
+        </div>
+    )
+}
