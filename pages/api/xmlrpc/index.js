867cf13bfb05f9432752fa93736776899954836b
terwer <youweics@163.com>
2022-06-16 02:38:02 +0800

    feat:test xmlrpc cors

--- pages/api/xmlrpc/index.js
+++ pages/api/xmlrpc/index.js
@@ -3,9 +3,26 @@ const xmlParser = require('xml2json');
 import handleResponse from "./metaweblog-server"
 import API_CONSTANTS from "./constant";
 
+import Cors from 'cors'
+import initMiddleware from '../../lib/init-middleware'
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+    // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+    Cors({
+        // Only allow requests with GET, POST and OPTIONS
+        methods: ['GET', 'POST', 'OPTIONS'],
+    })
+)
+
 // api list
 // https://github.com/fshost/xrpc/blob/master/example/app.js
 export default async function handler(req, res) {
+    // Run cors
+    await cors(req, res)
+
+    // Rest of the API logic
+
     // ===============
     // handle request
     // ===============
