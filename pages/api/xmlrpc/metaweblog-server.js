be53510aee534967c9ede0eba05b3c27b986db8a
terwer <youweics@163.com>
2022-06-20 13:09:32 +0800

    feat:test newPage

--- pages/api/xmlrpc/metaweblog-server.js
+++ pages/api/xmlrpc/metaweblog-server.js
@@ -1,15 +1,18 @@
 import handleGetUsersBlogs from "./metaweblog/getUsersBlogs";
 import handleGetCategories from "./metaweblog/getCategories";
+import handleNewPost from "./metaweblog/newPost";
 import API_CONSTANTS from "./constant"
 
 export default async function handleResponse(reqMethodName, reqParams) {
     switch (reqMethodName) {
-        case API_CONSTANTS.GET_USERS_BLOGS:
+        case API_CONSTANTS.METAWEBLOG_GET_USERS_BLOGS:
             return await handleGetUsersBlogs(reqParams);
             break
-        case API_CONSTANTS.GET_CATEGORIES:
+        case API_CONSTANTS.METAWEBLOG_GET_CATEGORIES:
             return await handleGetCategories(reqParams);
             break
+        case API_CONSTANTS.METAWEBLOG_NEW_POST:
+            return await handleNewPost(reqParams)
         default:
             break
     }
