1aa59576b9d162f43291503166212d6d63474c6d
terwer <youweics@163.com>
2024-06-05 14:51:53 +0800

    feat: add prismjs support

--- pages/api/prismjs.ts
+++ pages/api/prismjs.ts
@@ -29,24 +29,28 @@ export default async function handler(
   const body = req.body
   let html = body.html
 
-  // properly parse and format all hightlighted
-  // code blocks on the server
-  const $ = cheerio.load(html)
-  const $codes = $("code[class^=language]")
-  if ($codes.length > 0)
-    $codes.each(function () {
-      const $code = $(this)
+  try {
+    // properly parse and format all hightlighted
+    // code blocks on the server
+    const $ = cheerio.load(html)
+    const $codes = $("code[class^=language]")
+    if ($codes.length > 0)
+      $codes.each(function () {
+        const $code = $(this)
 
-      const lang = $code.attr("class")?.replace("language-", "") ?? "text"
-      const code = decode($code.html())
-      // default loaded languages with prisma, skip to decrease build times
-      if (!["clike", "markup"].includes(lang)) {
-        loadLanguages([lang])
-      }
-      $code.html(Prism.highlight(code, Prism.languages[lang], code))
-      $code.addClass(`prism language-${lang}`)
-      html = $.html()
-    })
+        const lang = $code.attr("class")?.replace("language-", "") ?? "text"
+        const code = decode($code.html())
+        // default loaded languages with prisma, skip to decrease build times
+        if (!["clike", "markup"].includes(lang)) {
+          loadLanguages([lang])
+        }
+        $code.html(Prism.highlight(code, Prism.languages[lang], code))
+        $code.addClass(`prism language-${lang}`)
+        html = $.html()
+      })
+  } catch (e) {
+    console.error("prismjs parse error", e)
+  }
 
   json = {
     html: html,
